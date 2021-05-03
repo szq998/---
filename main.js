500be7b35de9a27e7b8ea043b8d1a9522b76be80
szq <szq001@foxmail.com>
2021-05-04 00:30:00 +0800

    almost finished

--- main.js
+++ main.js
@@ -1,25 +1,169 @@
-function getText() {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve.bind(null, 'Hello World!' + new Date()), 1000);
-    });
-}
+const getWeiboHot = require('./scripts/get-weibo-hot');
+const doWithTimeout = require('./scripts/do-with-timeout');
+
+const ITEM_FONT_SIZE = 13;
+const ITEM_PADDING = 2;
+const ITEM_HEIGHT = ITEM_FONT_SIZE + 2 * ITEM_PADDING;
 
 (async function () {
-    let text;
+    let items = null;
+    let date = null;
     try {
-        text = await getText();
+        items = await doWithTimeout(getWeiboHot, 10000);
+        date = new Date();
+        $cache.setAsync({
+            key: 'data',
+            value: { items, date },
+        });
     } catch (e) {
-        text = 'no text';
+        console.log(e);
+
+        const cached = $cache.get('data');
+        if (cached && cached.items && cached.date) {
+            ({ items, date } = cached);
+        }
     }
+
+    const entry = {
+        info: items,
+        date,
+    };
     $widget.setTimeline({
+        entries: [entry],
+        policy: { atEnd: true },
         render: (ctx) => {
-            console.log(ctx);
+            const {
+                entry: { info: items, date },
+                family,
+                isDarkMode,
+            } = ctx;
+
+            if (date === null) {
+                return {
+                    type: 'zstack',
+                    views: [
+                        renderBackground(),
+                        {
+                            type: 'text',
+                            props: { text: '加载中...', font: $font(35) },
+                        },
+                    ],
+                };
+            }
+
+            const itemPerColumn = Math.floor(
+                (ctx.displaySize.height - 10) / ITEM_HEIGHT
+            );
+            const numColumn = ctx.family === 0 ? 1 : 2;
+            const openInSafari = $widget.inputValue;
+            const link = 'https://s.weibo.com/top/summary';
             return {
-                type: 'text',
+                type: 'zstack',
                 props: {
-                    text: text,
+                    widgetURL: openInSafari ? link : getLinkOpenedInJSBox(link),
                 },
+                views: [
+                    renderBackground(),
+                    renderUpdatingTime(date, family, isDarkMode),
+                    renderHotNews(items, itemPerColumn, numColumn),
+                ],
             };
         },
     });
 })();
+
+function renderBackground() {
+    return {
+        type: 'gradient',
+        props: {
+            startPoint: $point(0, 0),
+            endPoint: $point(1, 1),
+            colors: [
+                $color('#FFDEE9', '#1D4350'),
+                $color('#B5FFFC', '#A43931'),
+            ],
+        },
+    };
+}
+
+function renderUpdatingTime(date, family, isDarkMode) {
+    const opacity = isDarkMode ? 0.2 : 0.1;
+    return {
+        type: 'vstack',
+        views: [
+            {
+                type: 'text',
+                props: {
+                    text: '更新于',
+                    opacity,
+                },
+            },
+            {
+                type: 'text',
+                props: {
+                    date,
+                    style: $widget.dateStyle.time,
+                    font: $font('bold', family === 0 ? 35 : 50),
+                    opacity,
+                },
+            },
+        ],
+    };
+}
+
+function renderHotNews(items, itemPerColumn, numColumn) {
+    const space = {
+        type: 'spacer',
+        props: { frame: { width: 10 } },
+    };
+    return {
+        type: 'hstack',
+        views: [
+            space,
+            ...[...Array(numColumn)].map((_, i) =>
+                renderColumn(
+                    items.slice(i * itemPerColumn, (i + 1) * itemPerColumn)
+                )
+            ),
+            space,
+        ],
+    };
+}
+
+function renderColumn(items) {
+    return {
+        type: 'vstack',
+        props: {
+            spacing: 0, // 0 spacing，通过增大item的height来分隔，以增大点触面积
+        },
+        views: items.map(renderItem),
+    };
+}
+
+function renderItem(item) {
+    const link = `https://s.weibo.com/weibo?q=%23${encodeURIComponent(
+        item
+    )}%23&Refer=top`;
+    const openInSafari = $widget.inputValue;
+    return {
+        type: 'text',
+        props: {
+            link: openInSafari ? link : getLinkOpenedInJSBox(link),
+            text: item,
+            font: $font(ITEM_FONT_SIZE),
+            color: $color('darkGray', 'white'),
+            lineLimit: 1,
+            frame: {
+                maxWidth: Infinity,
+                height: ITEM_HEIGHT,
+                alignment: $widget.alignment.leading,
+            },
+            minimumScaleFactor: 0.7,
+        },
+    };
+}
+
+function getLinkOpenedInJSBox(url) {
+    const script = `$safari.open('${url}')`;
+    return `jsbox://run?script=${encodeURIComponent(script)}`;
+}
